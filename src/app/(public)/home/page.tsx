e6eea4bc8a2e55f06b5aadff94f5eb5128d80878
pomboexe <supernerds300@gmail.com>
2025-11-06 21:45:48 -0400

    feat: add HeroSpotlight

--- src/app/(public)/home/page.tsx
+++ src/app/(public)/home/page.tsx
@@ -1,3 +1,35 @@
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+
+
 export default function HomePage() {
-  return <h1>home</h1>;
+
+  const spotlight = { name: "SHOW SERTANEJO AO VIVO", desc: "Venha curtir um show ao vivo que acontecerá no Mirante Lúcia Almeida" }
+
+
+  return (
+    <div className="flex flex flex-col">
+
+      <section
+        className="w-screen h-screen bg-cover bg-center bg-no-repeat bg-black/60"
+        style={{ backgroundImage: "linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('/mirante-hero.png')" }}
+      >
+        <div className="flex items-center justify-end h-full gap-10 me-72">
+
+          <div className="text-right max-w-sm text-justify">
+            <h2 className="text-4xl font-bold ">{spotlight.name}</h2>
+            <p className="text-2xl font-light">
+              {spotlight.desc}
+            </p>
+          </div>
+
+          <ArrowForwardIosIcon sx={{ fontSize: 40 }} />
+
+        </div>
+      </section>
+
+
+
+    </div>
+  )
 }
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className='flex flex-col items-center'>
+        <div className='flex'>
             <Header />
             {children}
         </div>
