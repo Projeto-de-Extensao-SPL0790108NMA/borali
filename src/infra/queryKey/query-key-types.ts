dfdf9c56d6a4b0d996b75601c614eec416db9793
Arthur Bassi <arthur.winiarski.ranger@outlook.com>
2025-11-08 22:24:36 -0400

    chore: add API config and query keys

--- src/infra/queryKey/query-key-types.ts
+++ src/infra/queryKey/query-key-types.ts
@@ -1,3 +1,9 @@
 import { CompanyListPayload } from "@/domain/company/company-types";
 
 export type CompanyListQueryKey = CompanyListPayload;
+
+export type EventListQueryKey = {
+  companyId: string;
+  page?: number;
+  per_page?: number;
+};
